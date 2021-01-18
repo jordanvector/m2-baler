361bb79443161e940b00b9725e3dacd03f8c9fa6
Jordan Victor Schinella <jvschin@gmail.com>
2021-01-18 10:08:25 -0500

    Update JsBundles.php

--- Block/JsBundles.php
+++ Block/JsBundles.php
@@ -5,9 +5,9 @@
  */
 declare(strict_types=1);
 
-namespace Magento\Baler\Block;
+namespace Ironman\Baler\Block;
 
-use Magento\Baler\Model\FileManager;
+use Ironman\Baler\Model\FileManager;
 use Magento\Framework\View\Element\Context;
 use Magento\Framework\Filesystem\DirectoryList;
 use Magento\Framework\View\Page\Config as PageConfig;
